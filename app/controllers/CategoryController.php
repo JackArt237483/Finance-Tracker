327dba5061a5c5146c9059002eda72013fbf7527
JackArt237483 <vip.artem4567@mail.ru>
2024-12-06 18:36:19 +0600

    make new code

--- app/controllers/CategoryController.php
+++ app/controllers/CategoryController.php
@@ -1,1 +1,22 @@
 <?php
+namespace App\controllers;
+
+use App\Models\CategoryModel;
+
+class CategoryController {
+    private $categoryModel;
+
+    public function __construct(CategoryModel $categoryModel) {
+        $this->categoryModel = $categoryModel;
+    }
+
+    public function index() {
+        return $this->categoryModel->getAll();
+    }
+
+    public function create($name) {
+        $this->categoryModel->create($name);
+        header('Location: /categories');
+    }
+}
+?>
--- app/controllers/TransactionController.php
+++ app/controllers/TransactionController.php
@@ -1,1 +1,23 @@
 <?php
+namespace App\controllers;
+
+use App\Models\TransactionModel;
+
+class TransactionController {
+    private $transactionModel;
+
+    public function __construct(TransactionModel $transactionModel) {
+        $this->transactionModel = $transactionModel;
+    }
+
+    public function index() {
+        return $this->transactionModel->getAll();
+        require_once  '../views/main.php';
+    }
+
+    public function create($amount, $category_id, $type, $description) {
+        $this->transactionModel->create($amount, $category_id, $type, $description);
+        header('Location: main.php/transactions');
+    }
+}
+?>
